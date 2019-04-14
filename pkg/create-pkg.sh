c0497aa98fdce228b90213aecf374551021e9d7c
PEB <pebdev@lavache.com>
2019-04-14 16:45:23 +0100

    Updated files of the debian package

--- pkg/create-pkg.sh
+++ pkg/create-pkg.sh
@@ -26,39 +26,54 @@ FILENAME="ads-catcher_$1_all.deb"
 echo "- Preparing files..."
 F_MD5=$SCRIPT_PATH/debian/DEBIAN/md5sums
 P_BIN=$SCRIPT_PATH/debian/usr/bin
-#P_ETC=$SCRIPT_PATH/debian/etc
+P_ETC=$SCRIPT_PATH/debian/etc
+P_LIB=$SCRIPT_PATH/debian/lib/systemd/system
+P_USR=$SCRIPT_PATH/debian/usr
 P_DOC=$SCRIPT_PATH/debian/usr/share/doc/ads-catcher
 P_MAN=$SCRIPT_PATH/debian/usr/share/man/man1
 
 # Clean previous folder
-if [ -d "$SCRIPT_PATH/debian/etc" ]; then
-  rm -rf $SCRIPT_PATH/debian/etc
+if [ -d "$P_ETC" ]; then
+  rm -rf $P_ETC
 fi
 
-if [ -d "$SCRIPT_PATH/debian/usr" ]; then
-  rm -rf $SCRIPT_PATH/debian/usr
+if [ -d "$P_LIB" ]; then
+  rm -rf $P_LIB
+fi
+
+if [ -d "$P_USR" ]; then
+  rm -rf $P_USR
 fi
 
 rm -f $SCRIPT_PATH/*.deb
 
 # Create needed directory
 mkdir -p $P_BIN
-#mkdir -p $P_ETC
+mkdir -p $P_ETC
+mkdir -p $P_LIB
 mkdir -p $P_DOC
 mkdir -p $P_MAN
 
 # Copy files
-cp $SCRIPT_PATH/../ads-catcher/ads-catcher $P_BIN
-#cp $SCRIPT_PATH/../ads-catcher/settings.txt $P_ETC/ads-catcher.cfg
+cp $SCRIPT_PATH/../ads-catcher/ads-catcher* $P_BIN
+cp $SCRIPT_PATH/../config/settings.txt $P_ETC/ads-catcher.cfg
+cp $SCRIPT_PATH/../systemd/*.service $P_LIB/
 cp $SCRIPT_PATH/copyright $P_DOC
-gzip -9nk $SCRIPT_PATH/ads-catcher.1 && mv $SCRIPT_PATH/ads-catcher.1.gz $P_MAN/
+gzip -9nkf $SCRIPT_PATH/../man/ads-catcher.1 && mv $SCRIPT_PATH/../man/ads-catcher.1.gz $P_MAN/
+gzip -9nkf $SCRIPT_PATH/../man/ads-catcher-blacklister.1 && mv $SCRIPT_PATH/../man/ads-catcher-blacklister.1.gz $P_MAN/
+gzip -9nkf $SCRIPT_PATH/../man/ads-catcher-unlocker.1 && mv $SCRIPT_PATH/../man/ads-catcher-unlocker.1.gz $P_MAN/
 
 # Compute md5
 cd $SCRIPT_PATH/debian
 md5sum usr/bin/ads-catcher > $F_MD5
-#md5sum etc/ads-catcher.cfg >> $F_MD5
+md5sum usr/bin/ads-catcher-blacklister >> $F_MD5
+md5sum usr/bin/ads-catcher-unlocker >> $F_MD5
+md5sum etc/ads-catcher.cfg >> $F_MD5
+md5sum lib/systemd/system/ads-catcher.service >> $F_MD5
 md5sum usr/share/doc/ads-catcher/copyright >> $F_MD5
 md5sum usr/share/man/man1/ads-catcher.1.gz >> $F_MD5
+md5sum usr/share/man/man1/ads-catcher-blacklister.1.gz >> $F_MD5
+md5sum usr/share/man/man1/ads-catcher-unlocker.1.gz >> $F_MD5
 cd - > /dev/null
 
 # Create package
